81269073d1aafe3e9dd010501083c0c4348f5564
Thiago <thiagolazari123@hotmail.com>
2025-07-04 13:22:06 -0300

    Adiciona operações de soma e subtração
    Implementa funções para realizar operações de soma e subtração.
    
    Remove arquivos desnecessários.
    
    Cria um novo arquivo principal para demonstrar o uso das funções.

---
+++ operacoes.py
@@ -0,0 +1,10 @@
+
+def adc(num1 , num2):
+    resultado = num1 + num2
+    return resultado
+
+
+def sub(numero1, numero2):
+    resultado = numero1 - numero2
+    return resultado
+
--- subtrair.py
+++
@@ -1,4 +0,0 @@
-
-def subtrair(numero1, numero2):
-    resultado = numero1 - numero2
-    return resultado
